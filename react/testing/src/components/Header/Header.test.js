c551f1cf2a2b15c28d020243c5d7e626e85cfe4b
deepakkumarshinde3 <deepakkumar.shinde3@gmail.com>
2023-05-11 08:57:45 +0530

    testing-day2

--- react/testing/src/components/Header/Header.test.js
+++ react/testing/src/components/Header/Header.test.js
@@ -1,10 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "./Header";
 
-it("my 1st test case", () => {
-  render(<Header />);
-  // query
-  let element = screen.getByText(/hi/i);
-  // assert
-  expect(element).toBeInTheDocument();
+// it("check for todo header", () => {
+//   render(<Header />);
+//   let element = screen.getByText(/deepak/i);
+//   expect(element).toBeInTheDocument();
+// });
+
+// it("check for props in header", () => {
+//   let title = "My App";
+//   render(<Header text={title} />);
+//   //   let element = screen.getByText(title);
+//   //   let element = screen.getAllByRole("heading");
+//   //   expect(element).toBeInTheDocument();
+//   let element = screen.getByTitle("my-title");
+//   //   expect(element).toBeInTheDocument();
+//   expect(element).toBeVisible();
+// });
+
+it("header must invisible on click", () => {
+  let title = "My App";
+  render(<Header text={title} />);
+  let heading = screen.getByTitle("my-title");
+  let button = screen.getByRole("button");
+
+  // click ===> events ==> fireEvent
+  fireEvent.click(button);
+  // head must me invisible
+  expect(heading).not.toBeVisible();
 });
