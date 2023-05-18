7b121540138ce523c1c0708f1a90fcf87f90c4a0
deepakkumarshinde3 <deepakkumar.shinde3@gmail.com>
2023-05-18 08:59:16 +0530

    useContext Hook

--- react/advance-hooks/src/App.jsx
+++ react/advance-hooks/src/App.jsx
@@ -1,24 +1,8 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-
+import Input from "./components/Input";
 function App() {
-  const [counters, setCounters] = useState([0, 10, 20]);
-  const updateCounter = (index) => {
-    counters[index] += 1;
-    setCounters([...counters]);
-  };
   return (
     <>
-      {counters.map((value, index) => {
-        return (
-          <Counter
-            key={index}
-            index={index}
-            count={value}
-            updateCounter={updateCounter}
-          />
-        );
-      })}
+      <Input />
     </>
   );
 }
